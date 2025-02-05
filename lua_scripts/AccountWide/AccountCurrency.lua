3384239cafb28e53e9e071a8fb540af51bd3877e
Aldori <m.baxter1122@yahoo.com>
2025-02-05 12:18:32 -0500

    Refactored Accountwide Currency
    - Removed the hardcoded currency itemIDs.  Instead, we will retrieve their IDs once and store them in a table.  This approach will future-proof the script if additional currencies are added later on without needing to manually input the itemID into the script.

--- lua_scripts/AccountWide/AccountCurrency.lua
+++ lua_scripts/AccountWide/AccountCurrency.lua
@@ -9,45 +9,28 @@ local ENABLE_ACCOUNTWIDE_CURRENCY = false
 local ANNOUNCE_ON_LOGIN = false
 local ANNOUNCEMENT = "This server is running the |cFF00B0E8AccountWide Currency |rlua script."
 
--- -- -------------------------------------------------------------------------------------------
+-- -------------------------------------------------------------------------------------------
 -- END CONFIG
--- -- -------------------------------------------------------------------------------------------
+-- -------------------------------------------------------------------------------------------
 
 if not ENABLE_ACCOUNTWIDE_CURRENCY then return end
 
--- Note: these ItemIDs are currently configured in CurrencyTypes.dbc for Dinkledork's Ashen Order repack, so your mileage may vary with custom currencies, etc.  Just add/remove any as necessary.
-local currencyItemIDs = {
-    12840,   -- Minion's Scourgestone
-    12841,   -- Invader's Scourgestone
-    12843,   -- Corruptor's Scourgestone
-    19182,   -- Darkmoon Faire Prize Ticket
-    20558,   -- Warsong Gulch Mark of Honor
-    20559,   -- Arathi Basin Mark of Honor
-    20560,   -- Alterac Valley Mark of Honor
-    21229,   -- Qiraji Lord's Insignia
-    22637,   -- Primal Hakkari Idol
-    29024,   -- Eye of the Storm Mark of Honor
-    29434,   -- Badge of Justice
-    37711,   -- Reward Points
-    37836,   -- Venture Coin
-    40752,   -- Emblem of Heroism
-    40753,   -- Emblem of Valor
-    41596,   -- Dalaran Jewelcrafter's Token
-    42425,   -- Strand of the Ancients Mark of Honor
-    43016,   -- Dalaran Cooking Award
-    43228,   -- Stone Keeper's Shard
-    43307,   -- Arena Points
-    43308,   -- Honor Points
-    43589,   -- Wintergrasp Mark of Honor
-    43949,   -- Lich Rune
-    44990,   -- Champion's Seal
-    45624,   -- Emblem of Conquest
-    47241,   -- Emblem of Triumph
-    49426,   -- Emblem of Frost
-    829434,  -- Badge of Glory
-    829435,  -- Badge of Courage
-    1000010, -- Quest Reward Tokens
-}
+local function FetchCurrencyItemIDs()
+    local itemIDs = {}
+    local query = WorldDBQuery("SELECT entry FROM item_template WHERE BagFamily = 8192")
+
+    if query then
+        repeat
+            local itemID = query:GetUInt32(0)
+            table.insert(itemIDs, itemID)
+        until not query:NextRow()
+    end
+
+    return itemIDs
+end
+
+-- Dynamically populate currency items and store them in memory
+local currencyItemIDs = FetchCurrencyItemIDs()
 
 local function FetchAccountCurrency(accountId, currencyId)
     local query = CharDBQuery(string.format("SELECT count FROM accountwide_currency WHERE accountId = %d AND currencyId = %d", accountId, currencyId))
@@ -58,9 +41,6 @@ local function UpdateAccountCurrency(accountId, currencyId, newCount)
     CharDBExecute(string.format("INSERT INTO accountwide_currency (accountId, currencyId, count) VALUES (%d, %d, %d) ON DUPLICATE KEY UPDATE count = %d", accountId, currencyId, newCount, newCount))
 end
 
--- Purpose of this function is to populate the accountwide_currency table if it is empty, usually when the table is first created and the first character login.
--- Once the table has been populated, this function should always return out early since checkQuery will have records and the function will not proceed.
--- If the table is empty, then it will check item_instance for a sum of currencies on the account and use those to populate the table.
 local function InitializeAccountCurrencyOnEmptyTable(accountId)
     local checkQuery = CharDBQuery("SELECT 1 FROM accountwide_currency LIMIT 1")
 
@@ -99,7 +79,7 @@ local function AccountWideCurrency(event, player)
 
         player:RegisterEvent(function(_, _, _, player)
             SyncCurrencyOnLogin(player, accountId)
-        end, 1000, 1) -- Delay of 1000 milliseconds (1 second) to ensure that the InitializeAccountCurrencyOnEmptyTable() function finishes populating the empty table
+        end, 1000, 1) -- Delay of 1sec to ensure that the InitializeAccountCurrencyOnEmptyTable() function finishes populating the empty table
     elseif event == 25 then
         for _, currencyId in ipairs(currencyItemIDs) do
             local playerCurrencyCount = player:GetItemCount(currencyId)
